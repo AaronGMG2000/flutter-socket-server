074e07d0d2320631b2b2990bf3e365d1fb6f973c
RUDY AARÓN GOPAL MARROQUÍN GARCIA <3075624920403@ingenieria.usac.edu.gt>
2023-05-25 20:44:27 -0600

    final capitulo 4

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,7 +1,17 @@
 const { io } = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Bon Jovi'));
+bands.addBand(new Band('Heroes del Silencio'));
+bands.addBand(new Band('Metallica'));
 
 io.on('connection', (client) => {
     console.log('Cliente conectado');
+    client.emit('active-bands', bands.getBands());
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
     });
@@ -14,5 +24,20 @@ io.on('connection', (client) => {
         // io.emit('nuevo-mensaje', payload); // emite a todos
         console.log('payload', payload);
         client.broadcast.emit('nuevo-mensaje', payload); // emite a todos menos al que lo emitió
+    });
+
+    client.on('vote-band', (payload) => {
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('add-band', (payload) => {
+        bands.addBand(new Band(payload.name));
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('delete-band', (payload) => {
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
     });
 });
